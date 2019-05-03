5920b45e9068bba3dcb049156e61bcf19dcdb0a5
Kyle <kylekovary@protonmail.com>
2019-05-02 22:27:07 -0700

    merged functions.R and global.R

--- global.R
+++ global.R
@@ -26,20 +26,6 @@ formatHTML <- function(origFile, newFile){
 }
 
 
-volPlotDataFun <- function(numerator, denominator, data, pvalueCut, foldChangeCut){
-  # This function takes in the RNAseq data, and calculates the fold change
-  # and pvalue from two conditions (numerator and denominator) and labels
-  # which genes are significant or not based on the user defined pvalue and
-  # fold change cutoffs.
-  
-  data %>% filter(Condition %in% c(numerator, denominator)) %>%
-    group_by(GeneName) %>% 
-    summarise(foldChange = mean(TPM[Condition == numerator], na.rm = T) / mean(TPM[Condition == denominator], na.rm = T),
-              pvalue = t.test(TPM[Condition == numerator], TPM[Condition == denominator], na.rm = T)$p.value) %>%
-    mutate(Hit = ifelse(pvalue <= pvalueCut & abs(log2(foldChange)) >= log2(foldChangeCut), TRUE, FALSE))
-  
-}
-
 ###########################
 #### p-value functions ####
 ###########################
